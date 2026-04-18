89263f98b2bdc9e49d3cc1657a26027ab235be94
Ezeike David Chukwunonso <114363324+Cr8tivedav@users.noreply.github.com>
2026-04-18 11:26:27 +0100

    fix: correct embed pattern syntax

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -7,11 +7,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/pressly/goose/v3"
 	_ "github.com/lib/pq" // Postgres driver
+	"github.com/pressly/goose/v3"
 )
 
-//go:embed ../../migrations/*.sql
+//go:embed migrations/*.sql
 var embedMigrations embed.FS
 
 func Connect() (*sql.DB, error) {
@@ -53,7 +53,7 @@ func Connect() (*sql.DB, error) {
 
 	log.Println("Running database migrations...")
 	// Point to "../../migrations" because that's where the files live relative to this file
-	if err := goose.Up(db, "../../migrations"); err != nil {
+	if err := goose.Up(db, "migrations"); err != nil {
 		return nil, fmt.Errorf("migration failed: %v", err)
 	}
 
